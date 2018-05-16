898197ea9ce4da596c07b199036e14d1f97924b4
Halim Lais <halimlais@hotmail.com>
2018-05-16 13:05:34 +0100

    Ball & Camera Updates

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+    public Vector3  launchVel;
+    Rigidbody ball;
+    AudioSource ballRollingSound;
+    
+
+    // Use this for initialization
+    void Start ()
+    {
+        ball = GetComponent<Rigidbody>();
+        ballRollingSound = GetComponent<AudioSource>();
+
+        Launch();
+
+    }
+
+    public void Launch()
+    {
+
+        ballRollingSound.Play();
+        ball.velocity = launchVel;
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
