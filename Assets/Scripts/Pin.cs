f1c55c5a9bcd4ee4131b95fa4b6e1d49a2770d30
Halim Lais <halimlais@hotmail.com>
2018-05-27 00:24:33 +0100

    Pin logic isStanding Funciton

---
+++ Assets/Scripts/Pin.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pin : MonoBehaviour
+{
+
+    public float standingThreshold = 5f;
+
+
+    private void Start()
+    {
+
+
+    }
+    private void Update()
+    {
+
+    }
+    public bool IsStanding()
+    {
+        float tiltX = (transform.eulerAngles.x < 180f) ? transform.eulerAngles.x : 360 - transform.eulerAngles.x;
+        float tiltZ = (transform.eulerAngles.z < 180f) ? transform.eulerAngles.z : 360 - transform.eulerAngles.z;
+        if (tiltX > standingThreshold || tiltZ > standingThreshold)
+        {
+            return false;
+        }
+        return true;
+
+
+        //public bool IsStanding()
+        //{
+        //    Vector3 rotationInEuler = transform.rotation.eulerAngles;
+        //    float tiltInX = Mathf.Abs(rotationInEuler.x);
+        //    float tiltInZ = Mathf.Abs(rotationInEuler.z);
+
+        //    if (tiltInX < standingThreshold && tiltInZ < standingThreshold)
+        //    {
+        //        return true;
+        //    }
+
+        //    return false;
+        //}
+
+    }
+}
---
+++ Assets/Scripts/PinSetter.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine;
+
+public class PinSetter : MonoBehaviour {
+
+	public Text standingDisplay;
+	// Use this for initialization
+	void Start () {
+
+	  
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		standingDisplay.text = CountStanding().ToString();
+		
+	}
+	int CountStanding()
+	{
+		int standingPin = 10;
+
+		foreach (Pin pin in GameObject.FindObjectsOfType<Pin>()) 
+		{
+			if (pin.IsStanding())
+			{
+				standingPin++;
+			}
+
+		}
+		return standingPin;
+	
+	}
+}
